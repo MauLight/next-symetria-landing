ca9a22fd630a7f06cc92726e2aac39d4a5e1cab5
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-04-01 08:47:36 -0300

    feat: add privacy policy to landing

---
+++ src/app/[locale]/privacy/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div className='w-full h-full min-h-screen'>
+            <iframe
+                title='Privacy policy'
+                className='w-full h-full min-h-screen border-0'
+                loading='lazy'
+                src='https://www.termsfeed.com/live/f42efcda-69e8-4763-8438-e54a864911e1'
+            ></iframe>
+        </div>
+    )
+}
