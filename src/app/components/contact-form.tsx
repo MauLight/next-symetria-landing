b5897fd5010877dfaa7e4e01be740ab3bcb449f9
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-04-03 04:13:50 -0300

    feat: add animation to form button

--- src/app/components/contact-form.tsx
+++ src/app/components/contact-form.tsx
@@ -1,14 +1,16 @@
 'use client'
 
-import { motion, MotionConfig, AnimatePresence } from 'motion/react'
-import { useEffect, useState } from 'react'
-import useMeasure, { RectReadOnly } from 'react-use-measure'
 import axios from 'axios'
 import validator from 'validator'
+import { useEffect, useState } from 'react'
+import { useTranslations } from 'next-intl'
+import useMeasure, { RectReadOnly } from 'react-use-measure'
+import { motion, MotionConfig, AnimatePresence } from 'motion/react'
+
+//* Components
 import Form from './form'
 import ErrorComponent from './ErrorComponent'
 import { Agent, randRange } from '../classes'
-import { useTranslations } from 'next-intl'
 
 const recaptchaSiteKey = process.env.NEXT_PUBLIC_GRECAPTCHA
 
@@ -21,6 +23,17 @@ declare global {
     interface Window {
         grecaptcha: GoogleReCaptcha
     }
+}
+
+const containerVariants = {
+    initial: {},
+    hover: {}
+}
+
+const childVariants = {
+    initial: { opacity: 1, scale: 0, borderRadius: '100%' },
+    hover: { opacity: 1, scale: 2, borderRadius: '100%', transition: { duration: 0.2 } },
+    exit: { opacity: 0 }
 }
 
 export default function ContactForm({ parent }: { parent: RectReadOnly }) {
@@ -156,7 +169,7 @@ export default function ContactForm({ parent }: { parent: RectReadOnly }) {
     return (
         <MotionConfig transition={transition}>
             <div className="relative w-full h-[700px] flex flex-col items-start pt-44 px-5">
-                <div className="mx-auto w-full max-w-md z-20">
+                <div className="mx-auto w-full max-w-lg z-20">
                     <div className="rounded-[25px] border border-sym-border bg-sym-card overflow-hidden">
                         {
                             hasError && (
@@ -167,7 +180,7 @@ export default function ContactForm({ parent }: { parent: RectReadOnly }) {
                             !hasError && (
                                 <>
                                     <div className="px-8 pt-8">
-                                        <p className="text-lg text-sym-text-primary capitalize">{status === "idle" || status === "saving" ? text('first') : text('second')}</p>
+                                        <p className="text-form-header">{status === "idle" || status === "saving" ? text('first') : text('second')}</p>
                                     </div>
 
                                     <motion.div
@@ -188,14 +201,14 @@ export default function ContactForm({ parent }: { parent: RectReadOnly }) {
                                                             onSubmit={handleSubmitRecaptcha}
                                                             className=""
                                                         >
-                                                            <p className="text-sm text-sym-text-secondary">
+                                                            <p className="text-form-main">
                                                                 {text('third')}
                                                             </p>
                                                             <div className="mt-3">
                                                                 <input
                                                                     value={email}
                                                                     onChange={({ target }) => { setEmail(target.value) }}
-                                                                    className="block w-full px-2 rounded border-none h-10 bg-[#fff] text-slate-900 outline-0"
+                                                                    className="block h-10 w-full px-2 text-indigo-500 border border-sym-border rounded-[6px] outline-0"
                                                                     placeholder="your@email.com"
                                                                 />
                                                             </div>
@@ -208,17 +221,27 @@ export default function ContactForm({ parent }: { parent: RectReadOnly }) {
                                                                 />
                                                             </div>
 
-                                                            <div className='text-sym-text-secondary mt-4 flex gap-x-2 items-center'>
-                                                                <input type="checkbox" id="privacyConsent" name="privacyConsent" required />
-                                                                <label className='text-[0.9rem]' htmlFor="privacyConsent">
+                                                            <div className='mt-4 flex gap-x-2 items-center'>
+                                                                <input className='mt-[2px]' type="checkbox" id="privacyConsent" name="privacyConsent" required />
+                                                                <label className='text-form-small' htmlFor="privacyConsent">
                                                                     I agree to the <a className='text-indigo-500 underline' href="/terms" target="_blank">terms & conditions</a> and <a className='text-indigo-500 underline' href="/privacy" target="_blank">privacy policy</a>
                                                                 </label>
                                                             </div>
 
-                                                            <div className="mt-8 text-right">
-                                                                <Form.Button className="rounded bg-indigo-500 px-5 py-2 text-[1rem] text-sym-text-primary">
-                                                                    {text('fourth')}
-                                                                </Form.Button>
+                                                            <div className="mt-8 flex justify-end">
+                                                                <motion.div
+                                                                    variants={containerVariants}
+                                                                    initial='initial'
+                                                                    whileHover='hover'
+                                                                    exit='exit'
+                                                                    className="relative group overflow-hidden w-[120px] rounded-[6px] border border-indigo-500 px-4 py-2">
+                                                                    <Form.Button className='w-full flex justify-center text-[1rem] text-indigo-500 group-hover:text-black z-20'>
+                                                                        {text('fourth')}
+                                                                    </Form.Button>
+                                                                    <motion.div
+                                                                        variants={childVariants}
+                                                                        className='absolute -top-10 left-0 w-[120px] h-[120px] flex justify-center items-center bg-indigo-500'></motion.div>
+                                                                </motion.div>
                                                             </div>
                                                         </Form>
                                                     </motion.div>
@@ -241,8 +264,8 @@ export default function ContactForm({ parent }: { parent: RectReadOnly }) {
                         }
                     </div>
                 </div>
-                <div className='absolute top-0 left-0 w-full h-full z-10 bg-radial from-transparent  to-black'></div>
-                <div className='absolute top-0 left-0 w-full h-full z-10 bg-radial from-transparent  to-black'></div>
+                <div className='absolute top-0 left-0 w-full h-full z-10 bg-radial from-transparent to-black'></div>
+                <div className='absolute top-0 left-0 w-full h-full z-10 bg-radial from-transparent to-black'></div>
                 <canvas className='absolute top-0 left-0 h-full' width={parent.width} height={700} id='canvas'></canvas>
             </div>
         </MotionConfig>
